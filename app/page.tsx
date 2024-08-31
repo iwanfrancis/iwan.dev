7e94320cec971410537b66e79a95802d3435a398
Iwan Francis <iwanfrancis@gmail.com>
2024-08-31 16:27:54 +0100

    chore: :wrench: setup linting
    Setup eslint with plugins for prettier, tailwind and
    a11y. Also configures husky to run linting on commit
    and to check commit message with commitlint

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl">iwan.dev</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+      <h1 className="text-6xl">Iwan Francis</h1>
     </main>
-  );
+  )
 }
