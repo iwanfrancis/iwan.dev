04992ac2d2bc5a249b554cba5a5920b826a5dae1
Iwan Francis <iwanfrancis@gmail.com>
2024-09-01 17:14:46 +0100

    style: :lipstick: switch to flat style

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -6,11 +6,24 @@ export default function Home() {
     <Container
       as="main"
       rootClassName="min-h-screen py-16 flex flex-col justify-center items-stretch"
-      className="flex min-h-[60vh] w-full flex-col items-center gap-y-4 rounded-lg bg-slate-50 p-8 text-center shadow-lg"
+      className="flex min-h-[60vh] w-full flex-col items-center"
     >
-      <h1 className="text-5xl font-semibold sm:text-7xl md:text-8xl">
+      <h1 className="mb-8 bg-slate-50 p-6 pb-0 text-5xl font-semibold leading-none sm:text-7xl md:text-8xl">
         Iwan Francis
       </h1>
+
+      <p className="bg-slate-50 px-6 pb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+
+      <p className="bg-slate-50 px-6 pb-4">
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
     </Container>
   )
 }
